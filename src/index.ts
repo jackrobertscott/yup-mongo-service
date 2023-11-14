fde9d50ef50d56d9048c20c25029ac0cb6958b67
Jack Scott <jack.rob.scott@gmail.com>
2023-11-14 20:55:23 +0800

    added tsdoc comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Collection, Document, Filter, ObjectId, SortDirection } from "mongodb"
 import * as yup from "yup"
 
+/**
+ * Creates a service with MongoDB integration and Yup validation.
+ * @param options - Configuration options for the service.
+ * @returns An object containing MongoDB service methods.
+ */
 export function createYupMongoService<
   T extends { id: string },
   X extends Record<string, Function>
@@ -14,15 +19,33 @@ export function createYupMongoService<
   return { ...core, ...data }
 }
 
+/**
+ * Core factory function for creating MongoDB services with Yup validation.
+ * @param yupSchema - The Yup schema for data validation.
+ * @param getCollection - Function to retrieve the MongoDB collection.
+ * @returns An object with core MongoDB operations and validation.
+ */
 export function createYupMongoCore<T extends { id: string }>(
   yupSchema: yup.ObjectSchema<T>,
   getCollection: () => Promise<Collection>
 ) {
   return {
+    /**
+     * Gets the MongoDB collection.
+     */
     getCollection,
 
+    /**
+     * Gets the Yup schema.
+     */
     getSchema: () => yupSchema,
 
+    /**
+     * Creates a single document in the collection.
+     * @param document - The document to create, excluding the ID.
+     * @param _id - Optional MongoDB ObjectId for the document.
+     * @returns The ID of the created document.
+     */
     async createOne(
       document: Partial<Omit<T, "id">>,
       _id?: ObjectId
@@ -35,6 +58,11 @@ export function createYupMongoCore<T extends { id: string }>(
       return result.insertedId.toString()
     },
 
+    /**
+     * Creates multiple documents in the collection.
+     * @param documents - An array of documents to create, excluding their IDs.
+     * @returns An array of IDs of the created documents.
+     */
     async createMany(
       documents: Array<Partial<Omit<T, "id">>>
     ): Promise<string[]> {
@@ -51,6 +79,12 @@ export function createYupMongoCore<T extends { id: string }>(
       return Object.values(result.insertedIds).map((id) => id.toString())
     },
 
+    /**
+     * Updates a single document by ID.
+     * @param id - The ID of the document to update.
+     * @param document - The partial document data for update.
+     * @returns The updated document, or null if not found.
+     */
     async patchOneById(
       id: string | ObjectId,
       document: Partial<Omit<T, "id">>
@@ -64,6 +98,10 @@ export function createYupMongoCore<T extends { id: string }>(
       return this.getOneById(id)
     },
 
+    /**
+     * Updates multiple documents by their IDs.
+     * @param documents - An array containing document IDs and their partial data for update.
+     */
     async patchManyById(
       documents: Array<{ id: string | ObjectId; data: Partial<Omit<T, "id">> }>
     ): Promise<void> {
@@ -82,12 +120,22 @@ export function createYupMongoCore<T extends { id: string }>(
       await collection.bulkWrite(operations)
     },
 
+    /**
+     * Deletes a single document by ID.
+     * @param id - The ID of the document to delete.
+     * @returns True if a document was deleted, false otherwise.
+     */
     async deleteOneById(id: string | ObjectId): Promise<boolean> {
       const collection = await getCollection()
       const result = await collection.deleteOne({ _id: new ObjectId(id) })
       return result.deletedCount > 0
     },
 
+    /**
+     * Retrieves a single document by ID.
+     * @param id - The ID of the document to retrieve.
+     * @returns The document if found, or null otherwise.
+     */
     async getOneById(id: string | ObjectId): Promise<T | null> {
       const collection = await getCollection()
       const document = await collection.findOne({ _id: new ObjectId(id) })
@@ -95,6 +143,11 @@ export function createYupMongoCore<T extends { id: string }>(
       return this.addVirtualId(document)
     },
 
+    /**
+     * Retrieves a single document based on a filter.
+     * @param filter - The filter criteria for the document.
+     * @returns The document if found, or null otherwise.
+     */
     async getOne(filter: Filter<Omit<T, "id">>): Promise<T | null> {
       const collection = await getCollection()
       const document = await collection.findOne(filter as Filter<Document>)
@@ -102,6 +155,12 @@ export function createYupMongoCore<T extends { id: string }>(
       return this.addVirtualId(document)
     },
 
+    /**
+     * Retrieves multiple documents based on a filter and options.
+     * @param filter - The filter criteria for the documents.
+     * @param options - Query options like limit, skip, and sort.
+     * @returns An array of documents that match the criteria.
+     */
     async getMany(
       filter: Filter<Omit<T, "id">>,
       options?: {
@@ -120,6 +179,12 @@ export function createYupMongoCore<T extends { id: string }>(
       const documents = await query.toArray()
       return documents.map(this.addVirtualId)
     },
+
+    /**
+     * Validates data against the Yup schema.
+     * @param data - The data to validate.
+     * @returns The validated data.
+     */
     async getValidatedData(data: any): Promise<any> {
       try {
         return await yupSchema.validate(data, {
@@ -133,10 +198,19 @@ export function createYupMongoCore<T extends { id: string }>(
       }
     },
 
+    /**
+     * Retrieves the schema without the ID field.
+     * @returns The Yup schema excluding the ID field.
+     */
     getSchemaWithoutId() {
       return yupSchema.omit(["id"])
     },
 
+    /**
+     * Adds a virtual 'id' field to a document.
+     * @param document - The document to add the 'id' field to.
+     * @returns The document with the added 'id' field.
+     */
     addVirtualId(document: any): T {
       return { ...document, id: document._id.toString() }
     },
